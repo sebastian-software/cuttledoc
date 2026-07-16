4a68a716d90181790e2e0576ae9e9692dd49ed86
Sebastian Werner <s.werner@sebastian-software.de>
2026-07-16 10:15:20 +0200

    chore(docs): migrate framework stack

--- packages/docs/react-router.config.ts
+++ packages/docs/react-router.config.ts
@@ -3,11 +3,12 @@ import { glob } from 'node:fs/promises'
 import { createGetUrl, getSlugs } from 'fumadocs-core/source'
 
 const getUrl = createGetUrl('/docs')
-const basename = process.env.BASE_PATH?.replace(/\/$/, '') || ''
+const basePath = process.env.BASE_PATH?.replace(/\/?$/, '/') || ''
 
 export default {
   ssr: false,
-  basename,
+  // Only set basename if BASE_PATH is provided (for production builds)
+  ...(basePath ? { basename: basePath } : {}),
   async prerender({ getStaticPaths }) {
     const paths: string[] = []
 
